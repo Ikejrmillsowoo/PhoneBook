537a09b44448a8380329a276ea83a2f630d7ed3a
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-03-23 14:07:58 -0400

    add all test completed

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -15,71 +15,10 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = new Map<String, List<String>>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean containsKey(Object key) {
-                return false;
-            }
-
-            @Override
-            public boolean containsValue(Object value) {
-                return false;
-            }
-
-            @Override
-            public List<String> get(Object key) {
-                return List.of();
-            }
-
-            @Override
-            public List<String> put(String key, List<String> value) {
-                return List.of();
-            }
-
-            @Override
-            public List<String> remove(Object key) {
-                return List.of();
-            }
-
-            @Override
-            public void putAll(Map<? extends String, ? extends List<String>> m) {
-
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public Set<String> keySet() {
-                return Set.of();
-            }
-
-            @Override
-            public Collection<List<String>> values() {
-                return List.of();
-            }
-
-            @Override
-            public Set<Entry<String, List<String>>> entrySet() {
-                return Set.of();
-            }
-        };
+        this.phonebook = map;
     }
-
     public PhoneBook() {
-        this(null);
+        this(new HashMap<>());
     }
 
     public void add(String name, String phoneNumber) {
@@ -103,11 +42,9 @@ public class PhoneBook {
     }
 
     public String reverseLookup(String phoneNumber)  {
-//        Map<V, K> inversedMap = new HashMap<V, K>();
-//        for (Entry<String, List> entry : this.phonebook.entrySet()) {
-//            inversedMap.put(entry.getValue(), entry.getKey());
-//        }
-//        return inversedMap;
+        for(String name : this.phonebook.keySet()){
+            System.out.println(name);
+        }
         return  null;
     }
 
@@ -116,6 +53,6 @@ public class PhoneBook {
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.phonebook;
     }
 }
